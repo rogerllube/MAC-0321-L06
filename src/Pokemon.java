06c15b506fc7aa3ac4eaf392b152b0a0e2a0c79c
Sane98 <gsanefuji@uol.com.br>
2019-04-26 11:05:37 -0300

    :DD

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -3,4 +3,9 @@ abstract public class Pokemon {
 	public String type;
 	public String[] move;
 	public int hp;
+	public int attack;
+	public int defense;
+	public int spAttack;
+	public int spDefense;
+	public int speed;
 }
