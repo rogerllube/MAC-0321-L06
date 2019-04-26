53b1cff9a289f43e1aae0480ed03a8ac4918e140
Sane98 <gsanefuji@uol.com.br>
2019-04-26 10:57:26 -0300

    vai

--- src/Controller.java
+++ src/Controller.java
@@ -1,84 +1,45 @@
 class EventSet {
+	private Event[] events = new Event[100];
+	private int index = 0;	
+	private int next = 0;
+	public void add(Event e) {
+		if(index >= events.length)
+			return; // (In real life, throw exception)
+		events[index++] = e;
+	}	
 
-private Event[] events = new Event[100];
+	public Event getNext() {
+		boolean looped = false;
+		int start = next;
+		do {
+			next = (next + 1) % events.length;
+			// See if it has looped to the beginning:
+			if(start == next) looped = true;
+			// If it loops past start, the list
+			// is empty:
 
-private int index = 0;
+			if((next == (start + 1) % events.length) && looped)
+				return null;
+		} while(events[next] == null);
+		return events[next];
+	}
 
-private int next = 0;
-
-public void add(Event e) {
-
-if(index >= events.length)
-
-return; // (In real life, throw exception)
-
-events[index++] = e;
-
+	public void removeCurrent() {
+		events[next] = null;
+	}
 }
-
-public Event getNext() {
-
-boolean looped = false;
-
-int start = next;
-
-do {
-
-next = (next + 1) % events.length;
-
-// See if it has looped to the beginning:
-
-if(start == next) looped = true;
-
-// If it loops past start, the list
-
-// is empty:
-
-if((next == (start + 1) % events.length)
-
-&& looped)
-
-return null;
-
-} while(events[next] == null);
-
-return events[next];
-
-}
-
-public void removeCurrent() {
-
-events[next] = null;
-
-}
-
-}
-
 
 public class Controller {
-
-private EventSet es = new EventSet();
-
-public void addEvent(Event c) { es.add(c); }
-
-public void run() {
-
-Event e;
-
-while((e = es.getNext()) != null) {
-
-if(e.ready()) {
-
-e.action();
-
-System.out.println(e.description());
-
-es.removeCurrent();
-
-}
-
-}
-
-}
-
+	private EventSet es = new EventSet();
+	public void addEvent(Event c) { es.add(c); }
+	public void run() {
+		Event e;
+		while((e = es.getNext()) != null) {
+			if(e.ready()) {
+				e.action();
+				System.out.println(e.description());
+				es.removeCurrent();
+			}
+		}
+	}
 } 
