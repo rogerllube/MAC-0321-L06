367d0bf88efb3c2cfaf53030ec994e4c0aff8179
Sane98 <gsanefuji@uol.com.br>
2019-04-27 02:44:31 -0300

    Dano adicionado

--- src/Attack.java
+++ src/Attack.java
@@ -1,4 +1,42 @@
-
 public class Attack {
+	
+	public int Dano (Move m, Pokemon a, Pokemon b) {
+		Tabela typeEf = new Tabela();
+		double danoD, atk, def;
+		int danoI;
+		
+		if(b.getType2() == -1) {
+			if(m.physical == 1) {
+				atk = a.getAttack();
+				def = b.getDefense();
+				danoD = (((42*m.damage*(atk/def))/50 + 2) * typeEf.tab[m.type][b.getType1()]);
+				danoI = (int) Math.round(danoD);
+				return danoI;
+			}
+			else {
+				atk = a.getSpAttack();
+				def = b.getSpDefense();
+				danoD = (((42*m.damage*(atk/def))/50 + 2) * typeEf.tab[m.type][b.getType1()]);
+				danoI = (int) Math.round(danoD);
+				return danoI;
+			}
+		}
+		else {
+			if(m.physical == 1) {
+				atk = a.getAttack();
+				def = b.getDefense();
+				danoD = (((42*m.damage*(atk/def))/50 + 2) * typeEf.tab[m.type][b.getType1()] * typeEf.tab[m.type][b.getType2()]);
+				danoI = (int) Math.round(danoD);
+				return danoI;
+			}
+			else {
+				atk = a.getSpAttack();
+				def = b.getSpDefense();
+				danoD = (((42*m.damage*(atk/def))/50 + 2) * typeEf.tab[m.type][b.getType1()] * typeEf.tab[m.type][b.getType2()]);
+				danoI = (int) Math.round(danoD);
+				return danoI;
+			}
+		}
+	}
 
 }
