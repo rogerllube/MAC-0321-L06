74549425496734f23cb29acc4fb03a03160fa23f
rogerllube <rogeriollube@usp.br>
2019-04-27 14:49:28 -0300

    Lista e Jogo v0.9

--- src/Jogo.java
+++ src/Jogo.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Jogo {
 
 	public static void main(String[] args) {
-		System.out.println("Bem vindo, Digite o seu nome de treinador");
+		System.out.println("Bem vindo, jogador 1. Digite o seu nome de treinador");
 		Scanner scanner = new Scanner(System.in);
 		String name = scanner.next();
 		boolean con = false;
@@ -21,6 +21,23 @@ public class Jogo {
 			}
 		
 		}
+		System.out.println("Bem vindo, jogador 2. Digite o seu nome de treinador");
+		String name2 = scanner.next();
+		con = false;
+		while(con == false) {
+			System.out.println("Seu nome e " + name2 +"? Digite s para confirmar e n para alterar.");
+			cfm = scanner.next();
+			if (cfm.equals("s")) {
+				con = true;
+				System.out.println("Bom jogo, " + name2);
+			}
+			else {
+				System.out.println("Digite seu nome de treinador");
+				name2 = scanner.next();
+			}
+		
+		}
+		
 
 		scanner.close();
 	}
