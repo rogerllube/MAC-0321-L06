49e0f310f79dc13256c457ca5a3e3580f85256c6
Sane98 <gsanefuji@uol.com.br>
2019-04-27 15:11:13 -0300

    move e pokemon v0.9

--- src/Move.java
+++ src/Move.java
@@ -1,13 +1,19 @@
 
 public class Move {
+	private String name;
 	private int type;
 	private int damage;
 	private int physical;
 	
-	public Move (int tipo, int damage, int physical) {
+	public Move (String nome, int tipo, int damage, int physical) {
 		type = tipo;
 		this.damage = damage;
 		this.physical = physical;
+		name = nome;
+	}
+	
+	public String getName() {
+		return name;
 	}
 	
 	public int getType() {
@@ -21,4 +27,6 @@ public class Move {
 	public int getPhysical() {
 		return physical;
 	}
+	
+	
 }
